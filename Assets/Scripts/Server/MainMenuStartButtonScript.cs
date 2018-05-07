70155976afe9359bc108e50d512cdd36fed3c46c
Greg Dorchies <gdorchies@gmail.com>
2018-05-06 21:02:45 -0400

    New scene, abstracted lasers into teams
    New 'team' asset now creates lasers on load based on team members. I believe this is a necessary level of abstraction for the finished product.
    
    Also created a new 'scene' named 'MainMenu' and renamed 'Main' to 'Gameplay' to create some structure to the game flow

---
+++ Assets/Scripts/Server/MainMenuStartButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuStartButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    var button = this.GetComponent<Button>();
+    button.onClick.AddListener(TaskOnClick);
+  }
+	
+  void TaskOnClick()
+  {
+    Debug.Log("Clicked Start Button!");
+    SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+  }
+}
---
+++ Assets/Scripts/Server/TeamBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeamBehavior : MonoBehaviour
+{
+  public GameObject Avatar0;
+  public GameObject Avatar1;
+
+  private bool isTeamAlive = true;
+
+  // Use this for initialization
+  void Start()
+  {
+    //Doing this works because the lazer doesn't have 'start' called until the first game 
+    //update after the one that initializes this one
+    var lazer = (GameObject)Instantiate(Resources.Load("Lazer"));
+    var lazerBehavior = lazer.GetComponent<LazerBehaviour>();
+    lazerBehavior.Tether(Avatar0, Avatar1);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
