8d6bb530c9f89007a3077e7d2f4fe89c8f627632
LonelyZach <zach@lonelydevelopment.com>
2018-06-12 19:27:04 -0400

    Adding a basic speed boost powerup.

---
+++ Assets/Scripts/Server/ActivePowerupBehaviour.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+using System.Linq;
+
+/// <summary>
+/// Network behaviour for powerups that are activley affecting the game state.
+/// </summary>
+public abstract class ActivePowerupBehaviour : NetworkBehaviour
+{
+  public Color ActivatingAvatarColor;
+
+  protected AvatarBehaviour ActivatingAvatar;
+
+  private bool _initialized = false;
+
+  private NetworkIdentity _networkIdentity;
+
+  private float TimeToLive = 10.0f;
+
+  // Use this for initialization
+  void Start()
+  {
+    _networkIdentity = GetComponent<NetworkIdentity>();
+    Initialize();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (_networkIdentity.isServer)
+    {
+      ServerUpdate();
+    }
+
+    if(_networkIdentity.isClient)
+    {
+      ClientUpdate();
+    }
+  }
+
+  public void Initialize()
+  {
+    _initialized = true;
+    ActivatingAvatar = FindObjectsOfType<AvatarBehaviour>().Single(x => x.startColor == ActivatingAvatarColor);
+    StartPowerupServer();
+    Rpc_StartPowerupClient();
+  }
+
+  private void ClientUpdate()
+  {
+    gameObject.transform.position = ActivatingAvatar.transform.position;
+  }
+
+  private void ServerUpdate()
+  {
+    TimeToLive -= Time.deltaTime;
+
+    if(TimeToLive < 0.0f)
+    {
+      EndPowerupServer();
+      Rpc_EndPowerupClient();
+      NetworkServer.Destroy(gameObject);
+    }
+  }
+
+  protected abstract void StartPowerupServer();
+
+  protected abstract void StartPowerupClient();
+
+  protected abstract void EndPowerupServer();
+
+  protected abstract void EndPowerupClient();
+
+  [ClientRpc]
+  public void Rpc_EndPowerupClient()
+  {
+    EndPowerupClient();
+  }
+
+  [ClientRpc]
+  public void Rpc_StartPowerupClient()
+  {
+    StartPowerupClient();
+  }
+}
--- Assets/Scripts/Server/MovementParticleBehaviour.cs
+++ Assets/Scripts/Server/MovementParticleBehaviour.cs
@@ -2,25 +2,25 @@
 
 public class MovementParticleBehaviour : MonoBehaviour {
 
-  private AvatarBehaviour _avatarBehaviour;
+  public AvatarBehaviour AvatarBehaviour;
+
   private ParticleSystem _particleSystem;
 
 	// Use this for initialization
 	void Start () {
     _particleSystem = gameObject.GetComponentInChildren<ParticleSystem>();
-    _avatarBehaviour = gameObject.transform.parent.GetComponent<AvatarBehaviour>();
   }
 	
 	// Update is called once per frame
 	void Update ()
   {
-    var movementDirection = _avatarBehaviour.PlayerForceAngleReadOnly + 90.0f;
+    var movementDirection = AvatarBehaviour.PlayerForceAngleReadOnly + 90.0f;
     if(movementDirection > 360.0f)
     {
       movementDirection -= 360.0f;
     }
 
-    if(!float.IsNaN(movementDirection) && _avatarBehaviour.PlayerForceIntensityReadOnly > 0.0f)
+    if(!float.IsNaN(movementDirection) && AvatarBehaviour.PlayerForceIntensityReadOnly > 0.0f)
     {
       _particleSystem.gameObject.SetActive(true);
       gameObject.transform.eulerAngles = new Vector3(0, 0, movementDirection);
