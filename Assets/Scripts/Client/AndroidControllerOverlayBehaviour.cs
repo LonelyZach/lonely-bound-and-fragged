cac5e2d4eb3a6d1a2e7fc1695ca51ffa1b245422
LonelyZach <zach@lonelydevelopment.com>
2018-06-29 20:17:50 -0400

    Adding an overlay when playing on an Andorid device.

---
+++ Assets/Scripts/Client/AndroidControllerOverlayBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidControllerOverlayBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    if (Application.platform != RuntimePlatform.Android)
+    {
+      gameObject.SetActive(false);
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
